5aa931d383c9739f5963ef67c782ece286bbc877
leileijng <56954752+leileijng@users.noreply.github.com>
2020-05-19 17:42:23 +0800

    jl

---
+++ Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KidZaniaPhotoPrintingAdminPortal.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/Controllers/PrintingManagementController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace KidZaniaPhotoPrintingAdminPortal.Controllers
+{
+    public class PrintingManagementController : Controller
+    {
+        public ActionResult Hardcopy()
+        {
+            ViewBag.Title = "Hardcopy Printint";
+            return View();
+        }
+        public ActionResult Others()
+        {
+            ViewBag.Title = "Keychain Magnet Printing";
+            return View();
+        }
+        public ActionResult PausedOrder()
+        {
+            ViewBag.Title = "PausedOrder";
+            return View();
+        }
+    }
+}
