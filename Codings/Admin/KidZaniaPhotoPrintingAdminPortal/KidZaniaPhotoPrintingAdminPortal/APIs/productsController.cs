9f9bcefc50d119c8da379f230147d6a4104a93f6
Franky <franky.18@ichat.sp.edu.sg>
2020-06-03 17:53:02 +0800

    tried upload file

--- Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/APIs/productsController.cs
+++ Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/APIs/productsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using KidZaniaPhotoPrintingAdminPortal.Models;
 using Newtonsoft.Json.Linq;
@@ -129,6 +132,51 @@ namespace KidZaniaPhotoPrintingAdminPortal.APIs
             {
                 return BadRequest("Unable to delete product");
             }
+        }
+        [HttpPost]
+        [Route("api/products/UploadFile")]
+        public Task<HttpResponseMessage> Post()
+        {
+            List<string> savedFilePath = new List<string>();
+            // Check if the request contains multipart/form-data
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            //Get the path of folder where we want to upload all files.
+            string rootPath = HttpContext.Current.Server.MapPath("~/Content/ProductPhoto");
+            var provider = new MultipartFileStreamProvider(rootPath);
+            // Read the form data.
+            //If any error(Cancelled or any fault) occurred during file read , return internal server error
+            var task = Request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<HttpResponseMessage>(t =>
+                {
+                    if (t.IsCanceled || t.IsFaulted)
+                    {
+                        Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                    }
+                    foreach (MultipartFileData dataitem in provider.FileData)
+                    {
+                        try
+                        {
+                            //Replace / from file name
+                            string name = dataitem.Headers.ContentDisposition.FileName.Replace("\"", "");
+                            //Create New file name using GUID to prevent duplicate file name
+                            string newFileName = Guid.NewGuid() + Path.GetExtension(name);
+                            //Move file from current location to target folder.
+                            File.Move(dataitem.LocalFileName, Path.Combine(rootPath, newFileName));
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            string message = ex.Message;
+                        }
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.Created, savedFilePath);
+                });
+            return task;
         }
     }
 }
