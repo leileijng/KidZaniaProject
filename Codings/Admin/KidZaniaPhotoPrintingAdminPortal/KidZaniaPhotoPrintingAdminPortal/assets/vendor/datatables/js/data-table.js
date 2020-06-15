a0c17e74a292f2fc94a3021fe1ffa87ac9d1c093
Franky <franky.18@ichat.sp.edu.sg>
2020-06-16 01:05:14 +0800

    fix datatable and gstedit

--- Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/assets/vendor/datatables/js/data-table.js
+++ Codings/Admin/KidZaniaPhotoPrintingAdminPortal/KidZaniaPhotoPrintingAdminPortal/assets/vendor/datatables/js/data-table.js
@@ -10,18 +10,7 @@ jQuery(document).ready(function($) {
 
     /* Calender jQuery **/
 
-    if ($("table.second").length) {
-
-        $(document).ready(function() {
-            var table = $('table.second').DataTable({
-                lengthChange: false,
-                buttons: ['copy', 'excel', 'pdf', 'print', 'colvis']
-            });
-
-            table.buttons().container()
-                .appendTo('#example_wrapper .col-md-6:eq(0)');
-        });
-    }
+    
 
 
     if ($("#example2").length) {
