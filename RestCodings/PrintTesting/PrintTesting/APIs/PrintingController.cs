948ff26ac26d0a01b8d6d01216890f71c8c5e06b
Franky <franky.18@ichat.sp.edu.sg>
2020-07-13 13:59:03 +0800

    Loading Images

--- RestCodings/PrintTesting/PrintTesting/APIs/PrintingController.cs
+++ RestCodings/PrintTesting/PrintTesting/APIs/PrintingController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Http;
@@ -15,9 +16,10 @@ namespace PrintTesting.APIs
 {
     public class PrintingController : ApiController
     {
+        [AllowAnonymous]
         [HttpPost]
         [Route("api/printing")]
-        public IHttpActionResult uploadFile(string printername)
+        public HttpResponseMessage uploadFile(string printername)
         {
             var httpContext = HttpContext.Current;
             try
@@ -31,26 +33,43 @@ namespace PrintTesting.APIs
                         HttpPostedFile httpPostedFile = httpContext.Request.Files[i];
                         if (httpPostedFile != null)
                         {
+                            string fileName = Guid.NewGuid().ToString("N") + "." + "jpg";
+                            string filePath = "~/Content/1.jpg";
 
 
-                            InstalledPrinter installedPrinter = new InstalledPrinter();
-                            installedPrinter.PaperName = "A4";
-                            installedPrinter.PrinterName = printername;
-                            Stream fs = httpPostedFile.InputStream;
-                            BinaryReader br = new BinaryReader(fs);
-                            byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                            //Create a PrintFile object with the image file
-                            PrintFile file = new PrintFile(System.Web.HttpContext.Current.Server.MapPath("~/Content/1.jpg"), httpPostedFile.FileName);
-                            //Create a ClientPrintJob and send it back to the client!
-                            ClientPrintJob cpj = new ClientPrintJob();
-                            //set file to print...
-                            cpj.PrintFile = file;
-                            cpj.ClientPrinter = new InstalledPrinter(printername);
-                            Debug.WriteLine("Hi:" + cpj.GetContent());
-                            System.Web.HttpContext.Current.Response.ContentType = "application/octet-stream";
-                            System.Web.HttpContext.Current.Response.BinaryWrite(cpj.GetContent());
-                            System.Web.HttpContext.Current.Response.End();
-                            return Ok();
+                            if (filePath != null)
+                            {
+                                PrintFile file = null;
+                                file = new PrintFile(System.Web.HttpContext.Current.Server.MapPath(filePath), fileName);
+
+
+                                ClientPrintJob cpj = new ClientPrintJob();
+                                cpj.PrintFile = file;
+                                cpj.ClientPrinter = new DefaultPrinter();
+
+                                System.Web.HttpContext.Current.Response.ContentType = "application/octet-stream";
+                                System.Web.HttpContext.Current.Response.BinaryWrite(cpj.GetContent());
+                                System.Web.HttpContext.Current.Response.End();
+                            }
+                            //InstalledPrinter installedPrinter = new InstalledPrinter();
+                            //installedPrinter.PaperName = "A4";
+                            //installedPrinter.PrinterName = printername;
+                            //Stream fs = httpPostedFile.InputStream;
+                            //BinaryReader br = new BinaryReader(fs);
+                            //byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                            ////Create a PrintFile object with the image file
+                            //PrintFile file = new PrintFile(System.Web.HttpContext.Current.Server.MapPath("~/Content/1.jpg"), httpPostedFile.FileName);
+                            ////Create a ClientPrintJob and send it back to the client!
+                            //ClientPrintJob cpj = new ClientPrintJob();
+                            ////set file to print...
+                            //cpj.PrintFile = file;
+                            //cpj.ClientPrinter = new InstalledPrinter(printername);
+                            //HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+                            //Stream stream = new MemoryStream(cpj.GetContent());
+                            //result.Content = new StreamContent(stream);
+                            //result.Content.Headers.ContentType =
+                            //    new MediaTypeHeaderValue("application/octet-stream");
+                            //return result;
                         }
 
                     }
@@ -58,9 +77,13 @@ namespace PrintTesting.APIs
             }
             catch (Exception e)
             {
-                return BadRequest();
+                HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+                return result;
             }
-            return BadRequest();
+            HttpResponseMessage result1 = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return result1;
         }
     }
 }
